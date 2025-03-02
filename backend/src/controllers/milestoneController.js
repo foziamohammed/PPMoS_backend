981e5a4ff88185fabf855e1704140719e2059564
EstifanosTaye320 <estifanostaye11@gmail.com>
2025-03-03 00:15:13 +0300

    milestone task

--- backend/src/controllers/milestoneController.js
+++ backend/src/controllers/milestoneController.js
@@ -1,60 +1,119 @@
 const Milestone = require("../models/milestone");
 
 const addMilestone = async (req, res) => {
-    try {
-        const { title, stage, description, requirement, dueDate, status } = req.body;
-        const milestone = await Milestone.create({ studentId: req.user.id, stage, requirement, title, description, dueDate, status });
-        res.status(201).json({ milestone });
-    } catch (error) {
-        console.error("Error adding milestone:", error);
-        res.status(500).json({ message: "Something went wrong." });
-    }
+  const { edit, id } = req.query;
+  console.log(edit, id);
+  try {
+    const { title, stage, description, requirement, dueDate, status } =
+      req.body;
+    const milestone =
+      edit === "true"
+        ? await Milestone.findOneAndUpdate(
+            { _id: id },
+            { title, stage, description, requirement, dueDate, status },
+            { new: true, runValidators: true }
+          )
+        : await Milestone.create({
+            studentId: id,
+            stage,
+            requirement,
+            title,
+            description,
+            dueDate,
+            status,
+          });
+    res.status(201).json({ milestone });
+  } catch (error) {
+    console.error("Error adding milestone:", error);
+    res.status(500).json({ message: "Something went wrong." });
+  }
 };
 
 const getMilestone = async (req, res) => {
-    try {
-        const milestone = await Milestone.findOne({ studentId: req.user.id });
-        if (!milestone) {
-            return res.status(404).json({ message: "Milestone not found" });
-        }
-        res.status(200).json({ milestone });
-    } catch (error) {
-        res.status(500).json({ message: "Something went wrong." });
+  try {
+    const milestone = await Milestone.findOne({ studentId: req.user.id });
+    if (!milestone) {
+      return res.status(404).json({ message: "Milestone not found" });
     }
+    res.status(200).json({ milestone });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong." });
+  }
+};
+
+const getMilestoneById = async (req, res) => {
+  const { id } = req.params;
+  req.user.id = id;
+
+  try {
+    const milestone = await Milestone.findOne({ studentId: req.user.id });
+    if (!milestone) {
+      return res.status(404).json({ message: "Milestone not found" });
+    }
+    res.status(200).json({ milestone });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong." });
+  }
 };
 
 const updateMilestone = async (req, res) => {
-    try {
-        const { title, stage, description, requirement, dueDate, status } = req.body;
-        const milestone = await Milestone.findOneAndUpdate(
-            { studentId: req.user.id },
-            { title, stage, description, requirement, dueDate, status },
-            { new: true, runValidators: true }
-        );
-        if (!milestone) {
-            return res.status(404).json({ message: "Milestone not found" });
-        }
-        res.status(200).json({ milestone });
-    } catch (error) {
-        res.status(500).json({ message: "Something went wrong." });
+  try {
+    const { title, stage, description, requirement, dueDate, status } =
+      req.body;
+    const milestone = await Milestone.findOneAndUpdate(
+      { studentId: req.user.id },
+      { title, stage, description, requirement, dueDate, status },
+      { new: true, runValidators: true }
+    );
+    if (!milestone) {
+      return res.status(404).json({ message: "Milestone not found" });
     }
+    res.status(200).json({ milestone });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong." });
+  }
 };
 
 const updateMilestoneStatus = async (req, res) => {
-    try {
-        const { status } = req.body;
-        const milestone = await Milestone.findOneAndUpdate(
-            { studentId: req.user.id },
-            { status },
-            { new: true, runValidators: true }
-        );
-        if (!milestone) {
-            return res.status(404).json({ message: "Milestone not found" });
-        }
-        res.status(200).json({ milestone });
-    } catch (error) {
-        res.status(500).json({ message: "Something went wrong." });
+  try {
+    const { status } = req.body;
+    const milestone = await Milestone.findOneAndUpdate(
+      { studentId: req.user.id },
+      { status },
+      { new: true, runValidators: true }
+    );
+    if (!milestone) {
+      return res.status(404).json({ message: "Milestone not found" });
     }
+    res.status(200).json({ milestone });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong." });
+  }
 };
 
-module.exports = { addMilestone, getMilestone, updateMilestone, updateMilestoneStatus };
+const editMilestone = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const { title, stage, description, requirement, dueDate, status } =
+      req.body;
+    const milestone = await Milestone.findOneAndUpdate(
+      { _id: id },
+      { title, stage, description, requirement, dueDate, status },
+      { new: true, runValidators: true }
+    );
+
+    res.status(201).json({ milestone });
+  } catch (error) {
+    console.error("Error adding milestone:", error);
+    res.status(500).json({ message: "Something went wrong." });
+  }
+};
+
+module.exports = {
+  addMilestone,
+  getMilestone,
+  getMilestoneById,
+  updateMilestone,
+  updateMilestoneStatus,
+};
--- backend/src/routes/milestoneRoutes.js
+++ backend/src/routes/milestoneRoutes.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {  verifyToken } = require("../middlewares/authMiddleware")
-const { addMilestone, getMilestone, updateMilestone } = require('../controllers/milestoneController');
+const { verifyToken } = require("../middlewares/authMiddleware");
+const {
+  addMilestone,
+  getMilestone,
+  getMilestoneById,
+  updateMilestone,
+} = require("../controllers/milestoneController");
 
-router.post('/add', verifyToken, addMilestone);
-router.get('/get', verifyToken, getMilestone);
-router.put('/update', verifyToken, updateMilestone);
+router.post("/add", verifyToken, addMilestone);
+router.get("/get", verifyToken, getMilestone);
+router.get("/get/:id", verifyToken, getMilestoneById);
+router.put("/update", verifyToken, updateMilestone);
 
 module.exports = router;
